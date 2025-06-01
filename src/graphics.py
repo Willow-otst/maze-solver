af9ea8e5c7a8ba2e39dbd10a44a183456fec9a44
Willow <107652695+Willow-otst@users.noreply.github.com>
2025-06-01 13:49:24 -0500

    added points and line drawing

--- src/graphics.py
+++ src/graphics.py
@@ -12,6 +12,10 @@ class Window:
 
         print("window created...")
 
+    def drawLine(self, line, fillColor):
+        line.draw(self.__canvas, fillColor)
+
+
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -25,3 +29,18 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fillColor):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fillColor, width=2
+        )
--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from window import Window
+from graphics import Window, Point, Line
 
 def main():
+    lines = [
+        Line(Point(100, 100), Point(200, 100)),
+        Line(Point(200, 100), Point(200, 100)),
+        Line(Point(100, 300), Point(200, 100)),
+        Line(Point(100, 100), Point(300, 200)),
+        Line(Point(100, 100), Point(200, 300))
+    ]
+
     win = Window(800, 600)
+    for line in lines:
+        win.drawLine(line, "Black")
     win.wait_for_close()
 
 if __name__ == "__main__":
